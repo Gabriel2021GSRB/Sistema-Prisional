83242a488325346e41d3a1cfcca138d660a7fe72
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-03-07 22:04:21 -0300

    agora vamos incerir os operadores logicos

---
+++ presidioSp.py
@@ -0,0 +1,12 @@
+import datetime # aqui eu importei a biblioteca de hora da linguagem
+soldado = input("Qual seu QRA?")
+matricula = float (input("Qual a sua matricula?"))
+horario = float(input("Qual o horário do seu turno?"))
+entrada = float(input("Digite o horário da primeira honda:"))
+ronda = 4.0
+proxima_ronda = 0
+
+print(f"As hondas de celas são feitas de 4 em 4 horas {soldado} portador da matricula: {matricula}")
+#logo a cima dfinimos as variaveis, a baixo de agora em diante vai ser os comandos logicos para definir os horarios.
+entrada = (ronda) + (entrada)
+print ("o horario da proxima ronda é ", entrada)
