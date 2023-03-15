d0e2f257928c48a3236d71a6c407670f9085f34e
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-03-14 21:18:31 -0300

    contrução do while no sistema para obter repetição

--- presidioSp.py
+++ presidioSp.py
@@ -4,8 +4,33 @@ horario = float(input("Qual o horário do seu turno?"))
 entrada = float(input("Digite o horário da primeira honda:"))
 ronda = 4.0
 proxima_ronda = 0
-
-print(f"As hondas de celas são feitas de 4 em 4 horas {soldado} portador da matricula: {matricula}")
+continua = 'sim' 
+contador =0
+while 'sim':
+    print(f"As hondas de celas são feitas de 4 em 4 horas {soldado} portador da matricula: {matricula}")
 #logo a cima dfinimos as variaveis, a baixo de agora em diante vai ser os comandos logicos para definir os horarios.
-entrada = (ronda) + (entrada)
-print ("o horario da proxima ronda é ", entrada)
+   
+
+    if continua=='sim':
+       
+        entrada = (ronda) + (entrada)
+        print ("o horario da proxima ronda é ", entrada)
+
+    continua=str(input("digite sim ou não para continuar no sistema "))
+
+
+    
+    
+   
+ 
+    
+    
+
+    
+
+
+
+
+
+
+
