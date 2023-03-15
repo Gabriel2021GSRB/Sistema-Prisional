f02c4ebdc06e43b832f2ed04883a84064230111a
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-03-14 21:57:07 -0300

    organização.

--- presidioSp.py
+++ presidioSp.py
@@ -3,22 +3,18 @@ matricula = float (input("Qual a sua matricula?"))
 horario = float(input("Qual o horário do seu turno?"))
 entrada = float(input("Digite o horário da primeira honda:"))
 ronda = 4.0
-proxima_ronda = 0
 continua = 'sim' 
-contador =0
 #logo a cima dfinimos as variaveis, a baixo de agora em diante vai ser os comandos logicos para definir os horarios.
-#da linha dez em diante temos o while para fazer todas as repetições que o sistema precisar.
 while'sim'!='nao':
     print(f"As hondas de celas são feitas de 4 em 4 horas {soldado} portador da matricula: {matricula}")
 
-   
-
+#aqui foi utilizado as estruturas condicionais.
     if continua =='sim':
        
         entrada = (ronda) + (entrada)
         print ("o horario da proxima ronda é ", entrada)
 
-    continua=str(input("digite sim ou não para continuar no sistema: "))
+    continua=str(input("digite sim ou não para continuar no sistema "))
     
     if continua =='nao':
         print("voce saiu do sistema")
